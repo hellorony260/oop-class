df70f075ca41e388242a9c3c1f9f35e3e81a216a
Rony Rayhan <ronyrayhan260@gmail.com>
2024-02-18 13:04:50 +0600

    making a class and implement instantiate

---
+++ Inheritance/index.php
@@ -0,0 +1,10 @@
+<?php
+class Account
+{
+    public function deposite($amount)
+    {
+        return "depositing $amount tk in you account";
+    }
+}
+// $info = new Account();
+// echo $info->deposite(500);
