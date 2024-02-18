b99fb5e927861cb284bd8e8da018cb9a1c5bbcba
Rony Rayhan <ronyrayhan260@gmail.com>
2024-02-18 13:49:16 +0600

    abstraction with polymorphism

---
+++ Abstraction/index.php
@@ -0,0 +1,24 @@
+<?php
+abstract class Vehicles
+{
+    public abstract function baseFare();
+    public abstract function perkilo();
+    public function getTotal($kilo)
+    {
+        return $this->baseFare() + ($this->perkilo() * $kilo);
+    }
+}
+class Bike extends Vehicles
+{
+    public function baseFare()
+    {
+        return 50;
+    }
+    public function perkilo()
+    {
+        return 10;
+    }
+}
+
+$bike = new Bike();
+echo $bike->getTotal(5);
