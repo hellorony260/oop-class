c65e5c4d372e9d36bfc793ed1ce828e2f1693188
Rony Rayhan <ronyrayhan260@gmail.com>
2024-02-18 13:09:15 +0600

    inheritance

--- Inheritance/index.php
+++ Inheritance/index.php
@@ -8,3 +8,13 @@ class Account
 }
 // $info = new Account();
 // echo $info->deposite(500);
+class Check extends Account
+{
+    public function withdraw($amount)
+    {
+        return "withdrawing $amount tk from your account";
+    }
+}
+$check = new Check();
+echo $check->deposite(300) . PHP_EOL;
+echo $check->withdraw(100);
